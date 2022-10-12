573202f082870bf2d7f37dac3369ad5e29493836
Vicente Elias <vitt99@hotmail.com>
2022-10-12 16:14:54 -0500

    To_do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pdm2/Page/my_app_counter.dart';
+import 'package:pdm2/todo/my_app_todo.dart';
 
 void main() {
-  runApp(MyAppCounter());
+  runApp(MyApp());
 }
---
+++ lib/todo/todo_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ToDo {
+  final String task;
+
+  const ToDo({
+    required this.task
+  });
+}
+
+final task = [
+  new ToDo(task:'IA'),
+  new ToDo(task:'Movil'),
+];
